c576ba3ec984706c26cae53c831bdac166c16ab7
Marcoscab <marcos_stritzke@hotmail.com>
2022-05-08 12:39:39 -0300

    Proyecto Vacio, inicio

---
+++ src/app/models/persona.ts
@@ -0,0 +1,13 @@
+export interface Persona{
+
+    id:number;
+    nombre:string;
+    apellido:string;
+    fecha_nacimiento:Date;
+    domicilio: string;
+    acerca_de:string;
+    url_foto_perfil:string;
+    url_banner:string;
+
+
+}
